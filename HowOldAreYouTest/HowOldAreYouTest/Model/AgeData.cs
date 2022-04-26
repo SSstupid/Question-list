260b0c4fc83ae33df9878772b46702ff7c754b39
SSstupid <rlawoghks3@gmail.com>
2022-04-26 18:54:06 +0900

    Up

---
+++ HowOldAreYouTest/HowOldAreYouTest/Model/AgeData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowOldAreYouTest
+{
+    public class AgeData
+    {
+        public int age= 24;
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        } 
+    }
+}
---
+++ HowOldAreYouTest/HowOldAreYouTest/ViewModels/WindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace HowOldAreYouTest
+{
+    public class WindowViewModel : BaseViewModel
+    {
+        AgeData WhatAge = new AgeData();
+
+        public int GetSetAge { get; set; }
+
+        public WindowViewModel() => GetSetAge = WhatAge.Age;
+    }
+}
