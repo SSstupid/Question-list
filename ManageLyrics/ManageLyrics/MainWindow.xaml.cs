c1b1e3f960716496a1bab86cee4484339b83c42e
SSstupid <rlawoghks3@gmail.com>
2022-05-17 23:32:14 +0900

    Upload
    Test: ListView Template
    
    ListView Template 이용 시 테마의 문제로 적용이 안됨
    
    Related to #3

--- ManageLyrics/ManageLyrics/MainWindow.xaml.cs
+++ ManageLyrics/ManageLyrics/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ManageLyrics
 {
@@ -25,7 +12,7 @@ namespace ManageLyrics
         {
             InitializeComponent();
 
-            DataContext = new WindowViewModel();
+            DataContext = new ListViewViewModel();
         }
     }
 }
--- ManageLyrics/Model/FileData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ManageLyrics
-{
-    public class FileData
-    {
-        public string FileName { get; set; }
-        public string FullPath { get; set; }
-    }
-}
