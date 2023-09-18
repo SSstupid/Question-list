5c289d474de79012314c6068931287d45bc29dce
SSstupid <rlawoghks4@mangoslab.com>
2023-09-19 00:12:56 +0900

    제너릭 T 인스턴스 질문 업로드합니다.

---
+++ GenericTSampleApp/GenericTSampleApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericTSampleApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GenericTSampleApp/GenericTSampleApp/SampleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTSampleApp
+{
+    public class SampleViewModel : BaseViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
