7aba4e14666292ae8dcc84f0d9b0e89d35d31643
JaehwanMango <rlawoghks4@mangoslab.com>
2023-11-15 20:53:01 +0900

    custom control : SampleBorder

---
+++ SampleContextCanNotBeNull/SampleContextCanNotBeNull/CustomConstrol/SampleBorder.cs
@@ -0,0 +1,26 @@
+﻿using SkiaSharp.Views.Maui.Controls;
+
+namespace SampleContextCanNotBeNull;
+
+public class SampleBorder : Microsoft.Maui.Controls.Compatibility.Layout<View>
+{
+    // Events
+    // Properties
+    private readonly SKCanvasView _canvas;
+    // Constructors
+    public SampleBorder()
+    {
+        InputTransparent = true;
+        _canvas = new SKCanvasView
+        {
+            EnableTouchEvents = false,
+        };
+        Children.Add(_canvas);
+    }
+
+    protected override void LayoutChildren(double x, double y, double width, double height)
+    {
+        LayoutChildIntoBoundingRegion(_canvas, new Rect(x, y, width, height));
+    }
+}
+
